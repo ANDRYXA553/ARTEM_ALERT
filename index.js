ec857a1ff66224c0276266a281862f8ead2a9099
andriisyvyk <andrii.syvyk@agilie.com>
2024-02-19 17:36:59 +0200

    Artem bot

---
+++ index.js
@@ -0,0 +1,77 @@
+const {Client, GatewayIntentBits } = require('discord.js');
+const isToday = require('dayjs/plugin/isToday')
+const dayjs = require('dayjs')
+
+dayjs.extend(isToday)
+
+const token = process.env.token || 'MTIwOTA2MTY0ODI2Njk1Njg5MQ.G30zYD.mokk4GHQBzy2iiwqqtvOh4O5F_C8bgK454TssU';
+
+const { joinVoiceChannel,createAudioPlayer,createAudioResource,AudioPlayerStatus } = require('@discordjs/voice');
+
+const bohdanUserId = '862255677300277268'; // bodyana
+const andronUserId = '345604157723377676'; // miy
+const artemUserId = '343310025202204674'; // artema
+
+const monitoredChannelId = '973230021445570640';
+
+const messagesChannelId = '1209155717383323790';
+
+let todaysJoinDate = null
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    // if has been on server already
+    if(oldState.channelId === newState.channelId)return;
+    const channel = client.channels.cache.get(monitoredChannelId);
+
+    const messageChanel = client.channels.cache.get(messagesChannelId)
+
+
+    if (!channel) {
+        console.error('User or channel not found.');
+        return;
+    }
+
+    const isAnyOurId = newState.member.user.id === artemUserId;
+
+    if (newState.channel && newState.channel.id === monitoredChannelId && isAnyOurId) {
+
+        // has been logged today
+        if(todaysJoinDate !==null && dayjs().isToday(todaysJoinDate)) return;
+        todaysJoinDate = new Date();
+
+        const connection = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+
+        const player = createAudioPlayer();
+        const resource = createAudioResource('./audiofile.mp3');
+
+        const formattedDate = dayjs(todaysJoinDate).format('dddd, MMMM D, YYYY HH:mm')
+        messageChanel.send('ARTEM ZAISHOV SYOHODNI O \n' + formattedDate)
+
+        if(player.state.status !== 'playing' || player.state.status !== 'buffering') {
+            player.play(resource);
+            connection.subscribe(player);
+        }
+
+        player.on(AudioPlayerStatus.Idle, () => {
+            connection?.disconnect()
+        });
+    }
+});
+
+client.login(token);
